c46216eadc93fd750ed2795f91a6d462ce8b88ed
Anastasia <popugaychic@yandex.ru>
2022-09-20 15:30:56 +0500

    new task_13

--- sem_2/task_13/Program.cs
+++ sem_2/task_13/Program.cs
@@ -1,19 +1,11 @@
 ﻿using static System.Console;
 int num = int.Parse(ReadLine()!);
-while (num > 10) 
-{
-    if (num < 1000 && num > 99) 
+if (num > 99)
     {
-    num = num % 10;
-    WriteLine($"Третье число - {num}");
-    }
-    else if (num > 999) 
-    {
-   num = num / 10;
+        num = num / 100 % 10;
+        WriteLine(num);
     }
     else 
     {
         WriteLine("Третьей цифры нет");
-        break;
     }
-}
