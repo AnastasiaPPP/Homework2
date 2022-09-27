b5d449dcb6c10fbcf78c8084e8d8b1ab83fd7f6a
Anastasia <popugaychic@yandex.ru>
2022-09-27 12:28:53 +0500

    29 done

---
+++ sem_2/task_29/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+CreateArray();
+
+
+
+int[] CreateArray()
+{
+    int[] newArr = new int[8];
+    for (int i = 0; i < newArr.Length; i++)
+    {
+        newArr[i] = new Random().Next(1, 100);
+    }
+    for (int i = 0; i < newArr.Length; i++)
+    {
+        if (i == 0)
+        {
+            Write($"[{newArr[i]}, ");
+        }
+        else if (i == newArr.Length - 1)
+        {
+            Write($"{newArr[i]}]");
+        }
+        else 
+        {
+            Write($"{newArr[i]}, ");
+        }
+    }
+    return newArr;
+    
+}
