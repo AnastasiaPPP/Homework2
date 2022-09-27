d21c7e6b8731bbe4be95c36482834eba59b61653
Anastasia <popugaychic@yandex.ru>
2022-09-27 11:46:27 +0500

    25 done

---
+++ sem_2/task_25/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+int num1 = int.Parse(ReadLine()!);
+int num2 = int.Parse(ReadLine()!);
+int res = GetDegree(num1, num2);
+Write(res);
+
+int GetDegree(int x, int y)
+{
+    int count = 1;
+    int result = 1;
+    while (count <= y)
+    {
+        result *= x;
+        count++;
+    }
+    return result;
+}
