a09c8d1a03e78cdea6d1f26a99220d0e421386e4
Anastasia <popugaychic@yandex.ru>
2022-09-27 12:00:17 +0500

    27 done

---
+++ sem_2/task_27/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+int num = int.Parse(ReadLine()!);
+int finalNum = GetSum(num);
+Write(finalNum);
+
+
+int GetSum (int x)
+{
+    int rem = 0;
+    int res = 0;
+    while(x > 0)
+    {
+        rem = x % 10;
+        res += rem;
+        x /= 10;
+    }
+    return res;
+}
