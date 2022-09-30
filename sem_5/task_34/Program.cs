d9c29f90c4fb91897ec91ff0b8e934610701a9e2
Anastasia <popugaychic@yandex.ru>
2022-09-30 19:22:04 +0500

    task 34 done

---
+++ sem_5/task_34/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+WriteLine("Задайте длину массива:");
+int number = int.Parse(ReadLine()!);
+int[] array = MakeArray(number);
+String strArr = String.Join(", ", array);
+WriteLine(strArr);
+CountUp(array);
+
+
+
+int[] MakeArray(int num)
+{   Random rnd = new Random();
+    int[] array = new int[num];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+    }
+    return array;
+}
+void CountUp(int[] arr)
+{
+    int result = 0;
+    foreach (int item in arr)
+    {
+        if(item % 2 == 0) result += item;
+    }
+    WriteLine($"Сумма положительных чисел в массиве - {result}");
+}
