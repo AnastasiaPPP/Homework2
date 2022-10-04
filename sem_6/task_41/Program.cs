18bf3596540283306d90ccaec59f9cfc8ef010e1
Anastasia <popugaychic@yandex.ru>
2022-10-04 12:39:21 +0500

    task_41 done

---
+++ sem_6/task_41/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+WriteLine("Введите числа через пробел");
+string[] nums = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int[] nums2 = Array.ConvertAll(nums, int.Parse);
+int res = CountNums(nums2);
+Write($"Количество чисел больше ноля: {res}");
+
+int CountNums (int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] > 0) count +=1;
+    }
+    return count;
+}
+
+
