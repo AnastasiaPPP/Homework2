47b25bc511c3a0560eaaaf6c125d2c14e6254fd2
Anastasia <popugaychic@yandex.ru>
2022-10-11 13:42:38 +0500

    task 54 done

---
+++ sem_8/task54/Program.cs
@@ -0,0 +1,54 @@
+﻿using static System.Console;
+int n = int.Parse(ReadLine()!);
+int m = int.Parse(ReadLine()!);
+int[,] matr = GetMatrix(n, m);
+PrintMatrix(matr);
+WriteLine();
+SortingMatrix(matr);
+PrintMatrix(matr);
+
+int[,] GetMatrix(int rows, int cols)
+{
+    int[,] matrix = new int[rows, cols];
+    Random rnd = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Write($"{matrix[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+int[,] SortingMatrix (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1); k++)
+            {
+                if(matrix[i, j] <= matrix[i, k]) continue;
+                int temp = matrix[i, j];
+                matrix[i, j] = matrix[i, k];
+                matrix[i, k] = temp;
+            }
+            
+        }
+        
+    }
+return matrix;
+}
