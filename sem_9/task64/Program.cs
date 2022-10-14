300db7449f90c787a8c0d2e83ec5829788752b00
Anastasia <popugaychic@yandex.ru>
2022-10-14 12:01:24 +0500

    task 64 done

---
+++ sem_9/task64/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+
+WriteLine("Задайте число");
+int n = int.Parse(ReadLine()!);
+WriteLine(NumbersFromN(n));
+string NumbersFromN (int x)
+{
+    if(x == 1) return "1";
+    else return x + " " + NumbersFromN(x - 1);
+}
