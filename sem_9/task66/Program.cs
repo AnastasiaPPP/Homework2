35b1fb9894f3053e745612a6bb330fd7e14be571
Anastasia <popugaychic@yandex.ru>
2022-10-14 12:33:29 +0500

    task 66 done

---
+++ sem_9/task66/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+
+int x = int.Parse(ReadLine()!);
+int y = int.Parse(ReadLine()!);
+int res = GetSum(x, y);
+WriteLine(res);
+int GetSum(int m, int n)
+{
+    if(n == m) return m;
+    else return GetSum(m, n - 1) + n;
+}
