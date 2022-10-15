09ccce309a3630d488bb600d00f9b88757149d33
Anastasia <popugaychic@yandex.ru>
2022-10-15 14:44:40 +0500

    task 68 is done

---
+++ sem_9/task68/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+int m = int.Parse(ReadLine()!);
+int n = int.Parse(ReadLine()!);
+if(m < 0 || n < 0) return;
+WriteLine(AckermannFunc(m, n));
+
+int AckermannFunc (int m, int n)
+{
+    if(m == 0) return n + 1;
+    else if (m > 0 && n == 0) return AckermannFunc(m - 1, 1);
+    else return AckermannFunc(m - 1, (AckermannFunc(m, n - 1)));
+}
